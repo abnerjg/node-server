ebd1157c915b49b37dac78cb5b866bedea0f6d75
Abner Jimenez <ajjg21@gmail.com>
2023-09-21 13:44:01 -0600

    final

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 const tasks = [
-  { id: 1, description: 'Hacer las compras', completed: false },
-  { id: 2, description: 'Vender el coche', completed: true },
-  { id: 3, description: 'Estudiar node', completed: false },
+  { id: 1, description: 'Hacer las compras', completada: false },
+  { id: 2, description: 'Vender el coche', completada: true },
+  { id: 3, description: 'Estudiar node', pendiente: false },
 ];
 
 app.get('/tasks', (req, res) => {
